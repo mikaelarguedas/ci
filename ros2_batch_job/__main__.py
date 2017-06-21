0649c137f783e0bee936882915e391547904e9ce
Chris Lalancette <clalancette@openrobotics.org>
2017-06-21 11:31:17 -0400

    Do the right thing when there are no args.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- ros2_batch_job/__main__.py
+++ ros2_batch_job/__main__.py
@@ -241,7 +241,10 @@ def build_and_test(args, job):
             ['--cmake-args', '-DCMAKE_BUILD_TYPE=' +
                 args.cmake_build_type + ' --']
             if args.cmake_build_type else []
-        ) + args.ament_build_args + ['--'] +
+        ) +
+        (
+            [args.ament_build_args + '--'] if args.cmake_build_type else []
+        ) +
         (['--ament-cmake-args -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} ' +
             gcov_flags + ' " -DCMAKE_C_FLAGS="${CMAKE_C_FLAGS} ' +
             gcov_flags + '" --']
